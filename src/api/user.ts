2cf5bdd29146ba8ed439466478738db43fe087b4
Isabela Cioarec <bala.andreea@yahoo.com>
2024-02-14 21:50:40 +0200

    [main] Add register & authenticate requests.

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,9 +1,41 @@
-function authenticate(credentials) {
+import { ref } from 'vue';
+import { useFetch, createFetch } from '@vueuse/core';
 
+import type { Ref } from 'vue';
+import type { Credentials }  from '@/types/Credentials';
+
+const url: Ref<string> = ref('');
+
+const loggedInFetch = createFetch({
+  baseUrl: url,
+  options: {
+    async beforeFetch({ options }) {
+      const myToken = localStorage.getItem('token');
+      options.headers = {
+        ...options.headers,
+        Authorization: `Bearer ${myToken}`,
+      };
+
+      return { options }
+    },
+  },
+  fetchOptions: {
+    mode: 'cors',
+  },
+})
+
+async function authenticate(credentials: Credentials) {
+  const { isFetching, error, data } = await useFetch(url).post(credentials);
+  return data;
 }
 
+async function registerUser(user: Credentials) {
+  const { isFetching, error, data } = await useFetch(url).post(user);
+}
 
-function registerUser(user: any) {
+async function getDashboardData() {
+  const dashboardData = await loggedInFetch(url).get();
+  return dashboardData;
 }
 
 export const userApi = {
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue';
 
 import App from './App.vue';
 import router from './router';
+import 'bootswatch/dist/darkly/bootstrap.min.css';
 import { pinia } from './stores';
 
 const app = createApp(App);
